3964625c39487a3f8278712e790c5ea4b353ffe9
Vladimir Panteleev <vladimir@thecybershadow.net>
2013-12-19 18:04:31 +0000

    http: Fix <MOTD> syntax

--- http.d
+++ http.d
@@ -80,7 +80,7 @@ class WormNETHttpServer
 					case "Login.asp":
 						html = "<CONNECT %s>".format(configuration.irc.IP ? configuration.irc.IP : conn.conn.localAddress.toAddrString);
 						if (configuration.http.newsFileName.exists)
-							html ~= "\r\n<MOTD\r\n%s\r\n</MOTD>".format(configuration.http.newsFileName.readText);
+							html ~= "\r\n<MOTD>\r\n%s\r\n</MOTD>".format(configuration.http.newsFileName.readText);
 						break;
 					case "RequestChannelScheme.asp":
 						html = "<SCHEME=%s>".format(configuration.channels.aaGet(parameters.aaGet("Channel")).scheme);
